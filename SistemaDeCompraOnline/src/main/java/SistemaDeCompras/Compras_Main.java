e72ca1724e2669b289c93106d4862d48dd466fd8
João Gabriel <joaogabriel6392@gmail.com>
2026-05-20 14:22:14 -0300

    Add files via upload

---
+++ SistemaDeCompraOnline/src/main/java/SistemaDeCompras/Compras_Main.java
@@ -0,0 +1,19 @@
+package SistemaDeCompras;
+
+public class Compras_Main {
+    static void main() {
+        Compras_Class comprasClass= new Compras_Class();
+
+
+        comprasClass.addProduto();
+
+        comprasClass.dadosCliente();
+
+        comprasClass.CacularFrete();
+
+        comprasClass.cartao();
+
+
+
+    }
+}
